18f5b4a67ed5d6d0553a643dc8cfdf88b994a6f8
kate-cronin <97194571+kate-cronin@users.noreply.github.com>
2022-05-20 08:46:37 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,22 @@
+/*WRITE YOUR JAVASCRIPT FOR INPUT 1 HERE*/
+
+$(".button-blue").click(function() {
+let input1 = $(".input-blue").val();
+    console.log(input1);
+    $(".comment-blue").text(input1);
+});
+
+/*WRITE YOUR JAVASCRIPT FOR INPUT 2 HERE*/
+
+$().click(function() {
+
+});
+
+
+/*WRITE YOUR JAVASCRIPT FOR INPUT 3 HERE*/
+
+
+
+
+
+/*WRITE YOUR JAVASCRIPT FOR INPUT 4 HERE*/
